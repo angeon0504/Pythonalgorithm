4dfc27d2161b6fdc95a742256fc343ec5479b2fa
angeon <ckman92@naver.com>
2025-08-19 14:23:07 +0900

    LeetCode알고리즘 문제 풀기:Longest Consecutive

--- algorithm/LongestConsecutiveSequence/Main20250803.py
+++ algorithm/LongestConsecutiveSequence/Main20250803.py
@@ -2,11 +2,11 @@
 문제링크:https://leetcode.com/problems/longest-consecutive-sequence/description/
 제한시간:30분
 사용시간: 41분
-문제 내용:1.모든 숫자를 Set에 저장합니다.
+문제 내용:1.모든 숫자를 Set에 저장
 2.각 숫자에 대해, 해당 숫자가 연속된 수열의 시작인지 확인
 (num - 1)이 Set에 없다면, num이 수열의 시작임.
 3.수열의 길이를 하나씩 증가시키며 연속된 숫자를 찾기
-4.최대 길이를 저장해서 반환합니다.
+4.최대 길이를 저장해서 반환
 """
 
 def longestConsecutive(nums):
@@ -26,12 +26,10 @@ nums1 = [100, 4, 200, 1, 3, 2]
 print("Example 1 Input:", nums1)
 print("Output:", longestConsecutive(nums1))
 
-# 예제 2
 nums2 = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
 print("\nExample 2 Input:", nums2)
 print("Output:", longestConsecutive(nums2))
 
-# 예제 3
 nums3 = [1, 0, 1, 2]
 print("\nExample 3 Input:", nums3)
 print("Output:", longestConsecutive(nums3))
